5c26be3ef7c9e377c3b1335cf1f97463ce77ae38
Gertrud <eaagesi@students.eaaa.dk>
2018-10-08 08:49:01 +0200

    landingpage

---
+++ script.js
@@ -0,0 +1,21 @@
+// LANDINGPAGE
+//SLIDER
+
+  $(document).ready(function() {
+    $("#lightSlider").lightSlider();
+  });
+
+  $(document).ready(function() { var slider =
+ $("#content-slider").lightSlider({
+           keyPress:true,
+           speed: 1000,
+           auto: true,
+           loop: true,
+           thumbItem: 6,
+           pause: 5000,
+           item: 3,
+           centerSlide:true,
+           enableDrag:true,
+
+       });
+});
