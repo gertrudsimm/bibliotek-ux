93a34e17e45044c9be667b85985836ac6c775eba
Gertrud <eaagesi@students.eaaa.dk>
2018-10-08 09:20:44 +0200

    fikset

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-    var myDate = new Date(); /*Overkskrift til forsiden*/
-=======
+
 
 // LANDINGPAGE
 //SLIDER
@@ -23,24 +21,3 @@
 
        });
 });
-
-    var myDate = new Date();
->>>>>>> 27c31e75ed1678738c1aadb77550f0ce6be0a433
-    var hrs = myDate.getHours();
-
-    var greet;
-
-    if (hrs < 12)
-        greet = 'God morgen';
-    else if (hrs >= 12 && hrs <= 17)
-        greet = 'God eftermiddag';
-    else if (hrs >= 17 && hrs <= 24)
-        greet = 'God aften';
-
-  document.getElementById('lblGreetings').innerHTML =
-<<<<<<< HEAD
-        '<b>' + greet + '</b> og velkommen til biblioteket!'; 
-/* slut Overkskrift til forsiden*/
-=======
-        '<b>' + greet + '</b> and welcome to Encodedna.com!';
->>>>>>> 27c31e75ed1678738c1aadb77550f0ce6be0a433
